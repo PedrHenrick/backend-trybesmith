4bf1f347aced7c6daf6b039320e8a17acb8ad0af
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-07-09 00:33:22 -0300

    feat: :sparkles: Criando getAll no models da rota /users

--- src/models/users.model.ts
+++ src/models/users.model.ts
@@ -2,6 +2,12 @@ import { ResultSetHeader } from 'mysql2';
 import IUser from '../interfaces/user.inteface';
 import connection from './connection';
 
+const getAll = async (): Promise<IUser[]> => {
+  const query = 'SELECT * FROM Trybesmith.Users;';
+  const [rows] = await connection.execute(query);
+  return rows as unknown as IUser[];
+};
+
 const add = async (user: IUser): Promise<ResultSetHeader> => {
   const query = `INSERT INTO Trybesmith.Users (username, classe, level, password)
   VALUES(?, ?, ?, ?)`;
@@ -13,5 +19,6 @@ const add = async (user: IUser): Promise<ResultSetHeader> => {
 };
 
 export default {
+  getAll,
   add,
 };
