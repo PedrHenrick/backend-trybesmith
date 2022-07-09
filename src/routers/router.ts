3e71869c3efc4dc30f1e30d9b3b160aa13b3dc9f
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-07-09 00:40:47 -0300

    feat: :sparkles: Adicionando rota /login no arquivo de rotas

--- src/routers/router.ts
+++ src/routers/router.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import loginRouter from './loginRouter';
 import ordersRouter from './ordersRouter';
 import productsRouter from './productsRouter';
 import usersRouter from './usersRouter';
@@ -8,5 +9,6 @@ const routers = express.Router();
 routers.use('/products', productsRouter);
 routers.use('/users', usersRouter);
 routers.use('/orders', ordersRouter);
+routers.use('/login', loginRouter);
 
 export default routers;
