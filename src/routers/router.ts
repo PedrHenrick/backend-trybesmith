0556347a32f6e7caed2eb77499f5f43009e87a30
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-07-08 23:19:02 -0300

    feat: :sparkles: Adicionando /orders no arquivo que gerencia as rotas

--- src/routers/router.ts
+++ src/routers/router.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import ordersRouter from './ordersRouter';
 import productsRouter from './productsRouter';
 import usersRouter from './usersRouter';
 
@@ -6,5 +7,6 @@ const routers = express.Router();
 
 routers.use('/products', productsRouter);
 routers.use('/users', usersRouter);
+routers.use('/orders', ordersRouter);
 
 export default routers;
