aa9f621f232d95b95fc3d1faa634e9694072aab9
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-07-08 21:15:10 -0300

    feat: :sparkles: Criando método GET na rota /products e adicionando validação

--- src/routers/productsRouter.ts
+++ src/routers/productsRouter.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import productsController from '../controllers/products.controller';
+import validateProducts from '../middleware/validateProducts';
 
 const productsRouter = express.Router();
 
 productsRouter.get('/', productsController.getAll);
+productsRouter.post('/', validateProducts, productsController.addProduct);
 
 export default productsRouter;
