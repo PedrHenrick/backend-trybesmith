9c8898878b25a2bcc175e26b1b3167d1bc77f8a1
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-07-08 21:18:20 -0300

    feat: :sparkles: Adicionando middleware de erro no arquivo app
    Também foi adicionado a lib express-async-errors. Responsável por lidar com erros assíncronos do express

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
+import 'express-async-errors';
 import express from 'express';
 import routers from './routers/router';
+import ErrorMiddleware from './middleware/errorMiddleware';
 
 const app = express();
 
 app.use(express.json());
 app.use(routers);
-// app.use(errorMiddleware);
+app.use(ErrorMiddleware);
 
 export default app;
