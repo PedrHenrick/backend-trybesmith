906cc3aa8655dd81b86a005b37d13975f76374fc
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-07-08 21:13:19 -0300

    feat: :sparkles: Criando função addProduct na service da rota /products

--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -1,11 +1,16 @@
-import { getAll } from '../models/products.model';
+import productsModel from '../models/products.model';
 import IProducts from '../interfaces/product.interface';
 
 const getProducts = async (): Promise<IProducts[]> => {
-  const [row] = await getAll();
+  const [row] = await productsModel.getAll();
   return row as unknown as IProducts[];
+};
+const addProduct = async (product: IProducts): Promise<IProducts> => {
+  const { insertId } = await productsModel.add(product);
+  return { id: insertId, ...product };
 };
 
 export default {
   getProducts,
+  addProduct,
 };
