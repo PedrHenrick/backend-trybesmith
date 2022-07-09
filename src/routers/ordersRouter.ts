fd6e40f99f88a13580e9477cd4ef6f2e2a37ce96
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-07-09 17:45:01 -0300

    feat: :sparkles: Criando método post na rota /orders

--- src/routers/ordersRouter.ts
+++ src/routers/ordersRouter.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import ordersController from '../controllers/orders.controller';
+import authenticate from '../middleware/authenticationMiddleware';
+import validateSchemaOrders from '../middleware/validateOrders';
 
 const ordersRouter = express.Router();
 
 ordersRouter.get('/', ordersController.getAll);
+ordersRouter.post('/', authenticate, validateSchemaOrders, ordersController.addOrders);
 
 export default ordersRouter;
