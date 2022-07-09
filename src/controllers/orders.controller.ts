8ee52bf0b69f20f767571b6024b91cb9b98a93fc
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-07-09 17:47:02 -0300

    feat: :sparkles: Criando função addOrders no controller da rota /orders

--- src/controllers/orders.controller.ts
+++ src/controllers/orders.controller.ts
@@ -1,11 +1,21 @@
 import { Response, Request } from 'express';
 import ordersService from '../services/orders.service';
+import { authenticateToken } from '../utils/JWTToken';
 
 const getAll = async (_req: Request, res: Response): Promise<Response> => {
   const orders = await ordersService.getOrders();
   return res.status(200).json(orders);
 };
 
+const addOrders = async (req: Request, res: Response): Promise<Response> => {
+  const token = req.headers.authorization;
+  const userLogged = await authenticateToken(String(token));
+  const { productsIds } = req.body;
+  const orders = await ordersService.addOrders(productsIds, Object(userLogged));
+  return res.status(201).json(orders);
+};
+
 export default {
   getAll,
+  addOrders,
 };
