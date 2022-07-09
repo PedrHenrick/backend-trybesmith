1cb126354d1050847cbd09953c344379c7e0b073
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-07-09 17:47:43 -0300

    feat: :sparkles: Criando função updateItem no service da rota /products

--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -5,12 +5,19 @@ const getProducts = async (): Promise<IProducts[]> => {
   const [row] = await productsModel.getAll();
   return row as unknown as IProducts[];
 };
+
 const addProduct = async (product: IProducts): Promise<IProducts> => {
   const { insertId } = await productsModel.add(product);
   return { id: insertId, ...product };
 };
 
+const updateItem = async (productId: number, orderId: number) => {
+  await productsModel.updateItem(productId, orderId);
+  return productId;
+};
+
 export default {
   getProducts,
   addProduct,
+  updateItem,
 };
