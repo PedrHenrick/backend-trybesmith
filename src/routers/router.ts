58de63f80c9046cf545a7a9bfb2098dd4e65b57e
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-07-08 22:23:11 -0300

    feat: :sparkles: Adicionando rota /users no arquivo que gerencia as rotas

--- src/routers/router.ts
+++ src/routers/router.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import productsRouter from './productsRouter';
+import usersRouter from './usersRouter';
 
 const routers = express.Router();
 
 routers.use('/products', productsRouter);
+routers.use('/users', usersRouter);
 
 export default routers;
