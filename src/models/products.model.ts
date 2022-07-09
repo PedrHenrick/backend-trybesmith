2d999487de93da2631c5ade7d34f1ac078b1cb87
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-07-08 21:11:43 -0300

    feat: :sparkles: Criando função add na model da rota /products

--- src/models/products.model.ts
+++ src/models/products.model.ts
@@ -1,10 +1,21 @@
+import { ResultSetHeader } from 'mysql2';
 import IProducts from '../interfaces/product.interface';
 import connection from './connection';
 
-export const getAll = async (): Promise<IProducts[]> => {
+const getAll = async (): Promise<IProducts[]> => {
   const query = 'SELECT * FROM Trybesmith.Products;';
   const rows = await connection.execute(query);
   return rows as unknown as IProducts[];
 };
 
-export default getAll;
+const add = async ({ name, amount }: IProducts): Promise<ResultSetHeader> => {
+  const query = `INSERT INTO Trybesmith.Products (name, amount)
+  VALUES(?, ?)`;
+  const [result] = await connection.execute<ResultSetHeader>(query, [name, amount]);
+  return result;
+};
+
+export default {
+  getAll,
+  add,
+};
