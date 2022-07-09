f5a06e78978d864d8bcda09b65cb597d676ada42
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-07-09 17:49:09 -0300

    feat: :sparkles: Criando função addOrders no service da rota /orders

--- src/services/orders.service.ts
+++ src/services/orders.service.ts
@@ -1,12 +1,26 @@
+import IOrders from '../interfaces/order.interface';
 import ordersModel from '../models/orders.model';
+import productsService from './products.service';
 
-const getOrders = async () => {
+const getOrders = async (): Promise<IOrders[]> => {
   const orders = await ordersModel.getAll();
 
   return orders
     .map((order) => ({ ...order, productsIds: [order.productIds] }));
 };
 
+const addOrders = async (orders: number[], userLogged: unknown) => {
+  const { insertId } = await ordersModel.add(Number(Object(userLogged).id));
+  const productUpdated = await Promise.all(
+    orders.map((productId: number) => productsService.updateItem(productId, insertId)),
+  );
+  return {
+    userId: Number(Object(userLogged).id),
+    productsIds: productUpdated,
+  };
+};
+
 export default {
   getOrders,
+  addOrders,
 };
